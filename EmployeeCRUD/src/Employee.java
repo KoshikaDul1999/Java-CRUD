451e4da747c26c0cd432c498e066ab495f6d54c7
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-18 21:01:43 +0530

    create interface

---
+++ EmployeeCRUD/src/Employee.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Employee {
+    private JPanel Main;
+    private JTextField txtName;
+    private JTextField txtSalary;
+    private JTextField txtMobile;
+    private JButton saveButton;
+    private JTable table_1;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton searchButton;
+    private JTextField txtid;
+}
